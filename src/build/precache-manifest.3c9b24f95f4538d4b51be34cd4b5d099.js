af8563020531945154de9fe7f867ba55134bba49
Timm Ryan <ryan.timm@gmail.com>
2020-10-08 10:20:59 -0700

    first commit

---
+++ src/build/precache-manifest.3c9b24f95f4538d4b51be34cd4b5d099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35a143429442c9b321b6273d1cccb10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b4f48d524c292fa8350",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "8520178d2c49134ee522",
+    "url": "/static/js/2.bc37484a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bc37484a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b4f48d524c292fa8350",
+    "url": "/static/js/main.76c59fcf.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
